7059af467b365206b67ed0e9652816fd1a5445f1
saliq03 <javidsaliq@gmail.com>
2024-12-26 01:38:04 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/saliq/spotify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.saliq.spotify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/splash_page/pages/splash_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SplashPage(),
+    );
+  }
+}
+
