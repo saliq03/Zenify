156f21a040b94aaf90b1f13bce9f57c8f5ebfa67
saliq03 <javidsaliq@gmail.com>
2025-01-24 00:39:07 +0530

    search page

--- lib/presentation/search/pages/search.dart
+++ lib/presentation/search/pages/search.dart
@@ -10,63 +10,80 @@ class SearchPage extends StatelessWidget {
          appBar: const BasicAppbar(title: Text("Search",style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),),hideBack: true,),
         body: Column(
           children: [
+
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-              child: const TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 0)
-                ),
+              child: Row(
+                children: [
+                 Expanded (
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                        prefixIcon: Icon(Icons.search)
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Text("Cancel",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),)
+                ],
+                
               ),
             ),
-            Expanded(child: Container(
-              color: Colors.red,
-             child: ListView(
-               children: [
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
-                 Container(height: 20,width: double.infinity,color: Colors.white38,),
-                 SizedBox(height: 20,),
+            Expanded(child: ListView(
+              children: [
+                ListView.separated(
+                  shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context,index){
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: [
+                        Container(width: 50,height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.white38,
+                          shape: BoxShape.circle
+                        ),),
+                        const SizedBox(width: 10,),
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                          SizedBox(width: MediaQuery.of(context).size.width*0.6,
+                              child: Text("FKA twigs",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 16),)),
+                          const Text("Artist",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 14),)
+                        ],)
+                      ],
+                    ),
+                  );
 
-               ],
-             ),
+                }, separatorBuilder: (context,index)=>const SizedBox(height: 20,), itemCount: 5),
+                const SizedBox(height: 20,),
+                ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context,index){
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: [
+                            Container(width: 50,height: 50,
+                              decoration: const BoxDecoration(
+                                  color: Colors.white38,
+                              ),),
+                            const SizedBox(width: 10,),
+                            Column(crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(width: MediaQuery.of(context).size.width*0.6,
+                                  child: Text("Les",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 16),maxLines: 1, // Limit to a single line
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                                const Text("Song",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 14),)
+                              ],)
+                          ],
+                        ),
+                      );
+                    }, separatorBuilder: (context,index)=>const SizedBox(height: 20,), itemCount: 10),
+
+              ],
             ))
           ],
         ),
